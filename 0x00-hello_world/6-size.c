20a77adc9fe5649eb4909e55926574084d69d9b9
wael <waelbessaies2@gmail.com>
2022-02-03 15:28:44 +0100

    6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -17,5 +17,5 @@ int main(void)
 	printf("Size of a long int: %zu byte(s)\n", sizeof(c));
 	printf("Size of a long long int: %zu byte(s)\n", sizeof(d));
 	printf("Size of a float: %zu byte(s)\n", sizeof(e));
-	return (0);;
+	return (0);
 }
