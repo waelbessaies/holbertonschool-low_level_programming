e0eebb10de68f14b956f1496a0fc1b9811d824b5
wael <waelbessaies2@gmail.com>
2022-02-04 17:49:05 +0100

    7-print_tebahpla.c

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -1,8 +1,9 @@
 #include <stdio.h>
 /**
-* main - Entry point
-* Retun: Always 0
-*/
+ * main - Prints all the numbers of base 16 in lowercase.
+ *
+ * Return: Always 0.
+ */
 int main(void)
 {
 	char letter;
