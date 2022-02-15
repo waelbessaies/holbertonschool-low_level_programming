1b2fe3ef11a57b11d5cb4daa74a45ef6e4e81415
wael <waelbessaies2@gmail.com>
2022-02-15 11:08:44 +0100

    5-rev_string.c

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+  * rev_string - Reverses a string
+  * @s: The string to be modified
+  *
+  * Return: void
+  */
+void rev_string(char *s)
+{
+	int len = 0, i = 0;
+	char aux;
+
+	while (s[len] != '\0')
+		len++;
+
+	while (i < len--)
+	{
+		aux = s[i];
+		s[i++] = s[len];
+		s[len] = aux;
+	}
+}
+
