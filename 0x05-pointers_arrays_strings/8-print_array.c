4a925a3cb45b865aa3d0a5185248cedacda90ef5
wael <waelbessaies2@gmail.com>
2022-02-15 19:45:13 +0100

    8-print_array.c

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -1,23 +1,20 @@
 #include "main.h"
+#include <stdio.h>
 /**
- *print_array - str pointer
- *@a:pointer
- *@n:number of array
- * Return: On success 1.
- * On error, -1 is returned, and errno is set appropriately.
+ *print_array- prints  a string
+ * @a: input
+ * @n: input
+ * Return: void
  */
 void print_array(int *a, int n)
 {
 	int i;
 
-	for (i = 0 ; i < n ; i++)
+	for (i = 0; i < n; i++)
 	{
-	if (i == 0)
-	printf("%d", a[i]);
-	else
-	{
-	printf(", %d", a[i]);
-	}
+		printf("%d", a[i]);
+		if (i != n - 1)
+			printf(", ");
 	}
 	printf("\n");
 }
