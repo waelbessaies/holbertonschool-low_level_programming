ec79034d9dc6c6f0344684dc59485c433e568d82
wael <waelbessaies2@gmail.com>
2022-02-17 19:21:24 +0100

    2-strncpy.c

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ **_strncpy- prints  a string
+ * @dest: input
+ * @src: input
+ * @n: input
+ * Return: void
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i;
+
+	for (i = 0; i < n && src[i] != '\0'; i++)
+		dest[i] = src[i];
+	for ( ; i < n; i++)
+		dest[i] = '\0';
+
+	return (dest);
+}
