b00ef7a6f314e667b71c4de09cc2fcba6b07c45b
wael <waelbessaies2@gmail.com>
2022-02-17 20:18:41 +0100

    3-strcmp.c

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ **_strcmp- prints  a string
+ * @s1: input
+ * @s2: input
+ * Return: void
+ */
+int _strcmp(char *s1, char *s2)
+{
+	while ((*s1 != '\0' && *s2 != '\0') && (*s1 == *s2))
+	{
+		s1++;
+		s2++;
+	}
+	if (*s1 == *s2)
+	{
+		return (0);
+	}
+	else
+	{
+		return (*s1 - *s2);
+	}
+}
+
