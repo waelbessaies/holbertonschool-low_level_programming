d375bd41554dd8d8be3c09e1edcc50ca402e9962
wael <waelbessaies2@gmail.com>
2022-03-08 15:45:44 +0100

    dog.h

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -13,10 +13,5 @@ struct dog
 	char *name;
 	float age;
 	char *owner;
-};
-
-void init_dog(struct dog *d, char *name, float age, char *owner);
-void print_dog(struct dog *d);
-void free_dog(dog_t *d);
-
+}
 #endif
