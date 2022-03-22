63b4db1a0043ad51fd44b1a52e7101b6cb687883
wael <waelbessaies2@gmail.com>
2022-03-22 15:48:09 +0100

    frees2

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+/**
+ * free_listint2 - frees a listint_t list and stes the head to NULL
+ * @head: first element of the list
+ * Return: void
+ */
+void free_listint2(listint_t **head)
+{
+	listint_t *ptr;
+
+	if (head == NULL)
+		return;
+	while (*head)
+	{
+		ptr = *head;
+		*head = (*head)->next;
+		free(ptr);
+	}
+	*head = NULL;
+}
