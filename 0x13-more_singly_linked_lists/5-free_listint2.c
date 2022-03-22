6446dea2eea3290bf38b4e513449342b5d31a435
wael <waelbessaies2@gmail.com>
2022-03-22 15:59:23 +0100

    frees2

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -8,13 +8,12 @@ void free_listint2(listint_t **head)
 {
 	listint_t *ptr;
 
-	if (head == NULL)
-		return;
+	 *head = NULL;
+
 	while (*head)
 	{
 		ptr = *head;
 		*head = (*head)->next;
 		free(ptr);
 	}
-	*head = NULL;
 }
