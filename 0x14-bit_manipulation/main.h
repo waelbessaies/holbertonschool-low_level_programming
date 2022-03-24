b0435452dc12983e31871a576d0a5a2060822f40
wael <waelbessaies2@gmail.com>
2022-03-24 19:03:30 +0100

    1-print_binary.c

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -1,8 +1,11 @@
 #ifndef HEADER_FILE
 #define HEADER_FILE
 
+#include <unistd.h>
+#include <stdlib.h>
 #include <stddef.h>
 
+int _putchar(char c);
 unsigned int binary_to_uint(const char *b);
 void print_binary(unsigned long int n);
 int get_bit(unsigned long int n, unsigned int index);
