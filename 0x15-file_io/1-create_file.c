6724ca5717333f26eb84a7be2c8e21cd5df9d13b
wael <waelbessaies2@gmail.com>
2022-03-28 17:34:50 +0100

    1-create_file.c

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,32 @@
+#include "main.h"
+/**
+ * create_file-creates a file
+ * @filename: text file to read and printed
+ * @text_content: a NULL terminated string to write to the file
+ *
+ * Return: 1 on success, -1 on failure
+ */
+
+
+int create_file(const char *filename, char *text_content)
+{
+	int fd;
+
+	if (filename == NULL)
+		return (-1);
+
+	if (filename != NULL)
+	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
+	if (fd == -1)
+		return (-1);
+
+	if (text_content == NULL)
+	{
+		fd = open(filename, O_CREAT, 0600);
+		if (fd == -1)
+			return (-1);
+	}
+
+	close(fd);
+	return (1);
+}
