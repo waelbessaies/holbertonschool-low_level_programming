f2a39494bc25cfd8781852e6b49d27959114e716
wael <waelbessaies2@gmail.com>
2022-03-28 23:57:00 +0100

    1-create_file.c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,4 +1,7 @@
 #include "main.h"
+
+int _strlen(char *s);
+
 /**
  * create_file-creates a file
  * @filename: text file to read and printed
@@ -16,7 +19,7 @@ int create_file(const char *filename, char *text_content)
 		return (-1);
 
 	if (filename != NULL)
-	fd = open(filename, O_WRONLY & O_CREAT & O_TRUNC, 0600);
+	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
 	if (fd == -1)
 		return (-1);
 
@@ -25,10 +28,31 @@ int create_file(const char *filename, char *text_content)
 		fd = open(filename, O_CREAT, 0600);
 		if (fd == -1)
 			return (-1);
-		if (text_content != NULL)
-		write(fd, text_content, _strlen(text_content));
 	}
+	if (text_content != NULL)
+		write(fd, text_content, _strlen(text_content));
 
 	close(fd);
 	return (1);
 }
+
+
+/**
+ * _strlen - get length of string
+ *
+ * @s: string
+ *
+ * Return: length
+ **/
+int _strlen(char *s)
+{
+int i;
+i = 0;
+while (*(s + i) != '\0')
+{
+i++;
+}
+return (i);
+}
+
+
