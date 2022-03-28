04caeca0eba0c9e678b87b9638f27394fb355cb4
wael <waelbessaies2@gmail.com>
2022-03-28 23:54:21 +0100

    1-create_file.c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <string.h>
 /**
  * create_file-creates a file
  * @filename: text file to read and printed
@@ -16,7 +17,7 @@ int create_file(const char *filename, char *text_content)
 		return (-1);
 
 	if (filename != NULL)
-	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
+	fd = open(filename, O_WRONLY & O_CREAT & O_TRUNC, 0600);
 	if (fd == -1)
 		return (-1);
 
@@ -25,6 +26,8 @@ int create_file(const char *filename, char *text_content)
 		fd = open(filename, O_CREAT, 0600);
 		if (fd == -1)
 			return (-1);
+		if (text_content != NULL)
+		write(fd, text_content, _strlen(text_content));
 	}
 
 	close(fd);
