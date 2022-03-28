a9d44d09627a67eccf88ffa536dc574e138df006
wael <waelbessaies2@gmail.com>
2022-03-28 16:47:57 +0100

    0-read_textfile.c

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -3,13 +3,12 @@
  * read_textfile - reads a text file and prints it to
  * the POSIX standard output.
  * @filename: file to read and print
- * @letters: number if letters to read and print
+ *@letters: number if letters to read and print
  * Return: number of letters read and printed or 0 if failed
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
 	int openfile;
-	char *filecontent;
 	char *filecontent;
 	ssize_t read_content;
 	ssize_t write_content;
@@ -37,3 +36,4 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	free(filecontent);
 	close(openfile);
 	return (write_content);
+}
