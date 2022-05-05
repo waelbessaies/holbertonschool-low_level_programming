e268cf889ca4b2ca7c5bd00727c044e18b95e136
root <root@LAPTOP-I8GVERUB.localdomain>
2022-05-05 22:42:27 +0200

    1-dlistint_len.c

--- 0x17-doubly_linked_lists/1-dlistint_len.c
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -1,9 +1,9 @@
 #include "lists.h"
 
 /**
- * dlistint_len - returns number of elem in a linked list
- * @h: pointer
- * Return: number of elements
+* dlistint_len - returns number of elem in a linked list
+* @h: pointer
+* Return: number of elements
  */
 size_t dlistint_len(const dlistint_t *h)
 {
