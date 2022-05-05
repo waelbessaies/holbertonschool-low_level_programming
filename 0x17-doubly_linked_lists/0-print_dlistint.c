082590d8b599d5d375222a35998a45023427d38c
root <root@LAPTOP-I8GVERUB.localdomain>
2022-05-05 23:12:45 +0200

    0-print_dlistint.c

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -1,19 +1,20 @@
 #include "lists.h"
 
 /**
- * print_dlistint - print all elem of a list
- *@h : pointer
- * Return: nodes
+ * print_dlistint - prints all the elements of a list
+ * @h: head
+ * Return: number of nodes
  */
+
 size_t print_dlistint(const dlistint_t *h)
 {
-	size_t j = 0;
+	size_t i = 0;
 
 	while (h != NULL)
 	{
 		printf("%d\n", h->n);
 		h = h->next;
-		j++;
+		i++;
 	}
-	return (j);
+	return (i);
 }
