fa775c069094020e856ae35691c0d7fd18144143
root <root@LAPTOP-I8GVERUB.localdomain>
2022-05-05 20:50:32 +0200

    double linked list

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -1,20 +1,20 @@
 #include "lists.h"
 /**
  * print_dlistint - prints the elements of a list
- * @h: head 
+ * @h: head
  * Return: i
  */
 size_t print_dlistint(const dlistint_t *h)
 {
-	unsigned int i = 0;
+    unsigned int i = 0;
 
-	if (h == NULL)
-		return (i);
-	while (h)
-	{
-		printf("%d\n", h->n);
-		h = h->next;
-		i++;
-	}
-	return (i);
+    if (h == NULL)
+        return (i);
+    while (h)
+    {
+        printf("%d\n", h->n);
+        h = h->next;
+        i++;
+    }
+    return (i);
 }
