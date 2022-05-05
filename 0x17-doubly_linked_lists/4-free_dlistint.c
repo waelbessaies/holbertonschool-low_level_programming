822dd8ccbad37616d25356b22465cccda23571af
root <root@LAPTOP-I8GVERUB.localdomain>
2022-05-05 21:57:37 +0200

    4-free_dlistint.c

---
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -0,0 +1,16 @@
+#include "lists.h"
+/**
+ * free_dlistint - frees list
+ * @head: head of the list
+ */
+void free_dlistint(dlistint_t *head)
+{
+	dlistint_t *tmp;
+
+	while (head)
+	{
+		tmp = head->next;
+		free(head);
+		head = tmp;
+	}
+}
