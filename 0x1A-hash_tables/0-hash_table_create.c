1a16176d2e5aedd4e4e9c26585d01dd021342164
root <root@LAPTOP-I8GVERUB.localdomain>
2022-05-27 20:25:29 +0200

    0-hash_table_create.c

---
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -0,0 +1,21 @@
+#include "hash_tables.h"
+#include <stdlib.h>
+/**
+ * hash_table_create - creates a hash table
+ * @size: size of hash table
+ * Return: hash table
+ */
+hash_table_t *hash_table_create(unsigned long int size)
+{
+    unsigned long int i = 0;
+    hash_table_t *hashtable = malloc(sizeof(hash_table_t) * 1);
+
+    hashtable->array = malloc(sizeof(hash_node_t) * size);
+
+    for (; i < size; ++i)
+    {
+        hashtable->array[i] = NULL;
+    }
+
+    return (hashtable);
+}
